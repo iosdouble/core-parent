98deff96976917e3fb5b58c8c899f86abc318ad1
nihui <18202504057@163.com>
2019-10-16 18:21:42 +0800

    增加其他异常处理模块，增加web模块增加日志处理单元

---
+++ core-log/src/main/java/org/nh/core/log/contants/LogAppenderName.java
@@ -0,0 +1,10 @@
+package org.nh.core.log.contants;
+
+/**
+ * @Classname LogAppenderName
+ * @Description TODO
+ * @Date 2019/10/16 4:09 PM
+ * @Created by nihui
+ */
+public class LogAppenderName {
+}
